ee0b035c3ee32d78102229de891f8e0d05ef049a
Tikwa3 <128583712+Tikwa3@users.noreply.github.com>
2023-04-10 14:49:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
-world = input("Введите непонятное слово(большими буквами)")
-if world in meme_dict.keys():
-    print(meme_dict[world])
-else:
-    print("Такого слова пока что нет")
-        
+import discord
+from bot_logic import gen_pass
+from discord.ext import commands
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def films(ctx):
+    await ctx.send("Москва слезам не верит")
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("")
