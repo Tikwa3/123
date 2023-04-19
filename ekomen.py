6bf701b7826db7332b1015f60d5718ccebb876f7
Tikwa3 <128583712+Tikwa3@users.noreply.github.com>
2023-04-19 12:45:31 +0500

    Create ekomen.py

---
+++ ekomen.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+import os, random
+import requests
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я эко-мэн. я знаю команды: $plastik, $duck, $mem')
+
+def get_duck_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+@bot.command()
+async def mem(ctx):
+    img = random.choice(os.listdir("images"))
+    with open(f'images/{img}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def plastik(ctx):
+    img = random.choice(os.listdir("plastik"))
+    with open(f'plastik/{img}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+bot.run(" ")
